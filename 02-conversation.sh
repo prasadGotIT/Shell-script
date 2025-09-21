ee4f83c69fe4f547cb4b58e95e76a757ea81322a
prasad sripathi <prasad18218@gmail.com>
2025-09-21 14:47:03 +0530

    conversation file moved

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is a simple shell script that prints "Hello, World!" to the console.
+echo "Ramesh::Hi suresh, how are you?"
+echo "Suresh::I am fine, thank you! How about you?"
+echo "Ramesh::I am doing well, thanks for asking!"
+echo "Suresh::That's great to hear!"
+# End of script
