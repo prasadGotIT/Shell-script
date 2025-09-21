0cfef38f151e785cd795f4b7ca3f86f6839ea895
prasad sripathi <prasad18218@gmail.com>
2025-09-21 17:06:50 +0530

    Date in seconds

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #DATE=$(date +%Y-%m-%d)
+
 START_TIME=$(date +%s)
+
 sleep 10 &
+
 END_TIME=$(date +%s)
+
 TOTAL_TIME=$($END_TIME-$START_TIME)
 
 echo "script executed in: $TOTAL_TIME seconds"
