fb3e203f8850737983310cff1450d5d0cc75c81d
prasad sripathi <prasad18218@gmail.com>
2025-09-21 17:21:11 +0530

    Date in seconds

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "All variables passed to script: $@"
 echo "All variables passed to script: $*"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "who is running the script: $USER"
+echo "home directory of user: $HOME"
+echo "PID of current script: $$"
