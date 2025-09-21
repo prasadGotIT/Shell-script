9478c551b0b0207fffbcc87260ce70b0a28bda18
prasad sripathi <prasad18218@gmail.com>
2025-09-21 15:10:58 +0530

    names changed

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=John
+PERSON2=Raheem
+
+
+echo "$PERSON1::Hi $PERSON2, how are you?"
+echo "$PERSON2::I am fine, thank you! How about you?"
+echo "$PERSON1:I am doing well, thanks for asking!"
+echo "$PERSON2::That's great to hear!"
+# End of script
