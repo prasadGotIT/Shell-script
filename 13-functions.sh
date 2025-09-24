6b2c2238be526608400c6d49ac506d545e5cac74
prasad sripathi <prasad18218@gmail.com>
2025-09-24 23:35:11 +0530

    using function

---
+++ 13-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: Please run this script with ROOT access
+   exit 1
+fi 
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+   echo "ERROR:: $2 installation is failed
+   exit 1 
+else
+
+    echo " $2 installation is successfull"  
+fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
