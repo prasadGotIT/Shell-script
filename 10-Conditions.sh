9db95c4d105d4ad35adc87fd1b95aa536ad1a98c
prasad sripathi <prasad18218@gmail.com>
2025-09-24 22:26:44 +0530

    Conditions reverify

--- 10-Conditions.sh
+++ 10-Conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBER=$1
-if[($NUMBER -lt 10)] then;
-  echo "Given $NUMBER is less than 10"
+if [ $NUMBER -lt 10 ]; then
+  echo "Given number $NUMBER is less than 10"
 else
-  echo "Given $NUMBER is greaterthan or equal to 10"
+  echo "Given number $NUMBER is greaterthan or equal to 10"
 fi
-  
+
