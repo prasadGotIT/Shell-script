0ae0f495642fb4959c61ca1bd2f442544509ff80
prasad sripathi <prasad18218@gmail.com>
2025-09-24 22:49:03 +0530

    Even number check

---
+++ 11-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " please enter a number "
+read NUMBER
+
+if [ $NUMBER % 2 -eq 0 ]; then
+    
+   echo " Given number $NUMBER is Even"
+else
+   echo "Given number $NUMBER is odd"
+
+ fi     
