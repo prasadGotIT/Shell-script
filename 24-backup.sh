5cb21bfea6e5b0b1f130c86cae06a3629bc8ac8f
prasad sripathi <prasad18218@gmail.com>
2025-10-07 22:20:57 +0530

    backup

--- 24-backup.sh
+++ 24-backup.sh
@@ -7,6 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 SOURCE_DIR=$1
 DEST_DIR=$2
+DAYS=${3:-14}
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -39,4 +40,14 @@ if [ ! -d  $DEST_DIR ]; then
 echo -e "$R $DEST_DIR does not exist $N"
 exit 1
 
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime $DAYS)
+
+if [ ! -z "${FILES}" ]; then
+   echo "Files found"
+   TIMESTAMP=$(date +%F-%H-%M)
+   ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+else
+   echo "No files to archeive"
 fi
