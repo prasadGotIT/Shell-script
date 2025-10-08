a58e91c35065d94fc84d4433c94da6f099b4e60e
prasad sripathi <prasad18218@gmail.com>
2025-10-08 23:37:21 +0530

    backup

--- 24-backup.sh
+++ 24-backup.sh
@@ -55,6 +55,23 @@ if [ ! -z "${FILES}" ]; then
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     echo "Zip file name: $ZIP_FILE_NAME"
     find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"
+
+    ### Check Archieval Success or not ###
+    if [ -f $ZIP_FILE_NAME ];
+    then
+        echo -e "Archieval ... $G SUCCESS $N"
+
+        ### Delete if success ###
+        while IFS= read -r filepath
+        do
+           echo "Deleting the file: $filepath"
+           rm -rf $filepath
+           echo "Deleted the file: $filepath"
+        done <<< $FILES
+    else
+        echo "Archieval ... $R FAILURE $N"
+        exit 1
+    fi
 else
     echo -e "No files to archeive ... $Y SKIPPING $N"
 fi
