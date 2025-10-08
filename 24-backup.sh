2ddb669fc09774149f91f3f27949cbdca1bbf742
prasad sripathi <prasad18218@gmail.com>
2025-10-08 23:00:54 +0530

    backup

--- 24-backup.sh
+++ 24-backup.sh
@@ -42,7 +42,7 @@ exit 1
 
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime $DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 if [ ! -z "${FILES}" ]; then
    echo "Files found: $FILES"
