f1d9d0dd03adb1afa09224e99bf9f9b43e876954
prasad sripathi <prasad18218@gmail.com>
2025-10-11 00:01:33 +0530

    disk usage by mail

---
+++ 25-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 #in project we keep it as 75
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+
+while IFS= read -r line
+do
+   USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+   PARTITION=$(echo $line | awk '{print $7}')
+   if [ $USAGE -ge $DISK_THRESHOLD ]; then
+      MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>"
+   fi
+done <<< $DISK_USAGE
+
+echo -e "Message Body: $MESSAGE"
+
+sh mail.sh "prasad18218@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
